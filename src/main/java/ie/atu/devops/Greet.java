8ea676a71d904f5104438f950cecbdb614866962
Eren <eren@MacBookPro.station>
2025-10-15 16:37:53 +0100

    Switched to Java

---
+++ src/main/java/ie/atu/devops/Greet.java
@@ -0,0 +1,11 @@
+package ie.atu.devops;
+
+public class Greet {
+    public static String greet(int param) {
+        if (param == 1) {
+            return "Hello";
+        } else {
+            return "Goodbye";
+        }
+    }
+}
