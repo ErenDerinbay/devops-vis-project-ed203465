04c931fb0494af201b8bad68b35aede431a0029c
Eren <eren@MacBookPro.station>
2025-10-15 17:06:19 +0100

    Added javadoc

--- src/main/java/ie/atu/devops/Greet.java
+++ src/main/java/ie/atu/devops/Greet.java
@@ -1,6 +1,14 @@
 package ie.atu.devops;
 
+/**
+ * @author ErenDerinbay
+ * @version 1.0
+ */
 public class Greet {
+    /**
+     * @param param An integer value. If 1, returns "Hello"; otherwise returns "Goodbye".
+     * @return A string representing the greeting ("Hello" or "Goodbye").
+     */
     public static String greet(int param) {
         if (param == 1) {
             return "Hello";
