0f93ba1e5de4f88bcb4af0ba462ed9eeabe574f9
Eren <eren@MACBOOKPRO.station>
2025-11-13 12:11:19 +0000

    Fixed exclamation points missing in tests

--- src/test/java/ie/atu/devops/GreetTest.java
+++ src/test/java/ie/atu/devops/GreetTest.java
@@ -5,10 +5,10 @@ import junit.framework.TestCase;
 public class GreetTest extends TestCase {
 
     public void testGreetHello() {
-        assertEquals("Hello", Greet.greet(1));
+        assertEquals("Hello!", Greet.greet(1));
     }
 
     public void testGreetGoodbye() {
-        assertEquals("Goodbye", Greet.greet(2));
+        assertEquals("Goodbye!", Greet.greet(2));
     }
 }
